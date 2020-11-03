2c96314520a470ccd99257ab2de3fb8012f6d1b7
Tucker Hawkinson <7201606-tucker.hawkinson@users.noreply.gitlab.com>
2020-11-03 17:34:26 -0500

    messing with security banner

--- src-backend/main.js
+++ src-backend/main.js
@@ -13,8 +13,8 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
-      allowRunningInsecureContent: isDevEnv ? true : false
+      //   nodeIntegration: true,
+      allowRunningInsecureContent: false
     }
   });
 
