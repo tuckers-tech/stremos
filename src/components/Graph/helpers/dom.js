ef962f8ec25859117dd99ed09e55d81d763f669d
Tucker Hawkinson <7201606-tucker.hawkinson@users.noreply.gitlab.com>
2020-12-04 13:38:49 -0500

    prettiered helpers

--- src/components/Graph/helpers/dom.js
+++ src/components/Graph/helpers/dom.js
@@ -2,18 +2,18 @@
  * @param element {HTMLElement}
  * @return {{top: number, left: number}}
  */
-export function getOffsetRect (element) {
-  let box = element.getBoundingClientRect()
+export function getOffsetRect(element) {
+  let box = element.getBoundingClientRect();
 
-  let scrollTop = window.pageYOffset
-  let scrollLeft = window.pageXOffset
+  let scrollTop = window.pageYOffset;
+  let scrollLeft = window.pageXOffset;
 
-  let top = box.top + scrollTop
-  let left = box.left + scrollLeft
+  let top = box.top + scrollTop;
+  let left = box.left + scrollLeft;
 
-  return {top: Math.round(top), left: Math.round(left)}
+  return { top: Math.round(top), left: Math.round(left) };
 }
 
 export default {
   getOffsetRect
-}
+};
